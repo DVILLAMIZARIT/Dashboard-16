a78748a401e69066d2fc5c2b75197f7213c82210
Brad Christie <brad.christie@gmail.com>
2013-03-26 19:35:37 -0400

    Updated nuget packages
    Updated DotNetOAuth, jQuery, jQuery-UI Combined & jQuery Validate.

--- WebUI/Controllers/ErrorController.cs
+++ WebUI/Controllers/ErrorController.cs
@@ -1,19 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace WebUI.Controllers
 {
     public class ErrorController : Controller
     {
+        public ActionResult General(Exception exception)
+        {
+            return Content("General Error");
+        }
+
         //
         // GET: /Error/HttpError404
-        public ActionResult HttpError404()
+        public ActionResult HttpError404(Exception exception)
         {
             return Content("404: File not found");
         }
 
         //
         // GET: /Error/HttpError500
-        public ActionResult HttpError500()
+        public ActionResult HttpError500(Exception exception)
         {
             return Content("500: Server error");
         }
