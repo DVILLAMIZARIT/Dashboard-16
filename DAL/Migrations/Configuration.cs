ec035a28f7e6a46b1e32307af9f601c35737b19f
Brad Christie <brad.christie@gmail.com>
2013-07-28 11:36:02 -0400

    AppHarbor attempt

--- DAL/Migrations/Configuration.cs
+++ DAL/Migrations/Configuration.cs
@@ -2,7 +2,7 @@ namespace DAL.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DAL.DataContext>
+    public sealed class Configuration : DbMigrationsConfiguration<DAL.DataContext>
     {
         public Configuration()
         {
