f2ac3f2e7e2f3c601d1b5a2f2f10b4fe593f858d
Brad Christie <brad.christie@gmail.com>
2013-07-27 21:04:00 -0400

    Setting up for use with AppHarbor
    Added reference to connection string (provided by AppHarbor config
    setting) but still falls back to project-supplied web config when
    setting doesn't exist.

--- WebUI/Infrastructure/Installers/ContextInstaller.cs
+++ WebUI/Infrastructure/Installers/ContextInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using System;
+using Castle.MicroKernel.Registration;
 using DAL;
 using DAL.Interfaces;
 
@@ -8,8 +9,16 @@ namespace WebUI.Infrastructure.Installers
     {
         public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
         {
+
+            String connectionString = "DefaultConnection";
+            var appHarborConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLSERVER_CONNECTION_STRING"];
+            if (appHarborConnectionString != null)
+            {
+                connectionString = appHarborConnectionString.ConnectionString;
+            }
+
             container.Register(
-                Component.For<IDataContextFactory>().ImplementedBy<DataContextFactory>().DependsOn(Dependency.OnValue("nameOrConnectionString", "DefaultConnection")).LifestyleSingleton()
+                Component.For<IDataContextFactory>().ImplementedBy<DataContextFactory>().DependsOn(Dependency.OnValue("nameOrConnectionString", connectionString)).LifestyleSingleton()
             );
         }
     }
